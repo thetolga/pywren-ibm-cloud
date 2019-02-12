f34a9923d2835f28312e452c8f9f60f0da747cfd
Tolga O <tolga_oral@us.ibm.com>
2019-02-12 10:56:38 -0500

    Update deployutil.py
    exit() doesnt work with plain jyputer sys.exit() is needed

--- pywren/pywren_ibm_cloud/deployutil.py
+++ pywren/pywren_ibm_cloud/deployutil.py
@@ -38,7 +38,7 @@ def create_zip_action(pywren_location=None):
     except Exception as e:
         print(e)
     if res != 0:
-        exit()
+        sys.exit()
     os.remove(prefix + '/pywren/__main__.py')
 
 
