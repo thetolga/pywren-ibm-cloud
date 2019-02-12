ce48c158cf469b55100ab68a75d3dcd6ae9a3ffe
Tolga O <tolga_oral@us.ibm.com>
2019-02-12 11:38:17 -0500

    Update deployutil.py

--- pywren/pywren_ibm_cloud/deployutil.py
+++ pywren/pywren_ibm_cloud/deployutil.py
@@ -15,7 +15,7 @@
 #
 
 import os
-from site import exit
+import site
 from shutil import copyfile
 from pywren_ibm_cloud import wrenconfig
 from pywren_ibm_cloud.storage import storage
@@ -39,7 +39,7 @@ def create_zip_action(pywren_location=None):
     except Exception as e:
         print(e)
     if res != 0:
-        exit()
+        site.exit()
     os.remove(prefix + '/pywren/__main__.py')
 
 
